b68a273d1fc9a9a176a222e1c8d5f468ffc9bc27
Jamil Ahmed <89215004+jamil-001@users.noreply.github.com>
2024-09-30 21:20:14 +0600

    Add files via upload

---
+++ arithmaticOperator.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    // Addition
+    //printf("%d",2+3);
+    //substraction
+    //printf("%d",3-2);
+    //Multiplication
+   // printf("%d\n",2*3);
+   // Division
+    //printf("%d",15/3);
+    ///Modulaus
+    //printf("%d",5%3);
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%d",a+b);
+    return 0;
+}
